0a23843720d8ae6bf0c50504c08b553060a41263
DMC09 <Dharminlive12@live.com>
2020-10-02 18:34:23 -0500

    added support for cors

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const importData = require("./data.json");
 let port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("hello world!");
