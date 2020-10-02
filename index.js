aa9547da084081a96bb2570686706652d6917518
DMC09 <Dharminlive12@live.com>
2020-10-02 17:36:57 -0500

    changed port and profile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const importData = require('./data.json')
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8080;
 
 app.get("/", (req,res) => res.send('hello world!') )
 app.get("/players", (req,res) => res.send(importData) )
