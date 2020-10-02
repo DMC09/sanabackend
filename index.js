7d92f49ba2bfb7737202272485eff1dbe4e7c3e2
DMC09 <Dharminlive12@live.com>
2020-10-02 17:41:38 -0500

    added debugging logs

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const importData = require('./data.json')
+const importData = require("./data.json");
 let port = process.env.PORT || 8080;
 
-app.get("/", (req,res) => res.send('hello world!') )
-app.get("/players", (req,res) => res.send(importData) )
+app.get("/", (req, res) => {
+  res.send("hello world!");
+  console.log("this is a test for the base route");
+});
+app.get("/players", (req, res) => {
+  res.send(importData);
+  console.log("this is a test for the players route");
+});
 
-app.listen(port, ()=> {
-  console.log(`example app is listenining port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`example app is listenining port ${port}`);
+});
